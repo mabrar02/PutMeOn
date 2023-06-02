d5b0088c4fbc5059d8cd93ef41e6afb76875e05d
mabrar02 <mahdeen02@gmail.com>
2023-06-02 01:41:11 -0300

    clear button, empty list working

--- app/screens/YourMusic.js
+++ app/screens/YourMusic.js
@@ -1,37 +1,87 @@
-import { FlatList, StyleSheet, Text, View, } from 'react-native';
-import React, {useState, useEffect} from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import TestMusic from '../../components/TestMusic';
 import MusicComponent from '../../components/MusicComponent';
-import * as SecureStore from "expo-secure-store"
+import * as SecureStore from "expo-secure-store";
 import { FIREBASE_DB } from '../../firebaseConfig';
-import { ref, child, get } from 'firebase/database';
+import { ref, child, get, remove, onValue } from 'firebase/database';
 
 export default YourMusic = () => {
   const [songs, setSongs] = useState([]);
+  const [isDatabaseEmpty, setDatabaseEmpty] = useState(false);
 
   useEffect(() => {
-    fetchSongs();
+    const fetchData = async () => {
+      const userId = await SecureStore.getItemAsync('user_id');
+      const dbRef = ref(FIREBASE_DB, `users/${userId}/Music/YourMusic`);
+
+      const listener = onValue(dbRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          const songList = Object.values(data);
+          setSongs(songList);
+          setDatabaseEmpty(false);
+        } else {
+          setSongs([]);
+          setDatabaseEmpty(true);
+        }
+      });
+
+      return () => {
+        
+        listener();
+      };
+    };
+
+    fetchData();
   }, []);
 
-  const fetchSongs = async () => {
-    const userId = await SecureStore.getItemAsync('user_id');
-    const dbRef = ref(FIREBASE_DB, `users/${userId}/Music/YourMusic`);
-    const snapshot = await get(dbRef);
+  const clearAllSongs = async () => {
+    const userId = await SecureStore.getItemAsync('user_id').then(
+      (userId) => clearFromDB(userId)
+    );
+  };
 
-    if (snapshot.exists()) {
-      const data = snapshot.val();
-      const songList = Object.values(data);
-      setSongs(songList);
-    }
-  }
+  const clearFromDB = async (id) => {
+    remove(ref(FIREBASE_DB, `users/${id}/Music/YourMusic`));
+  };
+
+  const confirmClear = () => {
+    Alert.alert(
+      'Clear Songs',
+      'Are you sure you want to clear all your songs? This action is not reversible!',
+      [
+        { text: 'Cancel', style: 'cancel' },
+        { text: 'Clear', style: 'destructive', onPress: clearAllSongs },
+      ],
+      { cancelable: true }
+    );
+  };
+
+  const renderFooter = () => {
+    return (
+      songs.length > 0 &&
+      !isDatabaseEmpty && (
+        <TouchableOpacity style={styles.clearAllButton} onPress={() => confirmClear()}>
+          <Text style={styles.clearAllText}>Clear All</Text>
+        </TouchableOpacity>
+      )
+    );
+  };
 
   return (
     <View style={styles.container}>
+      {songs.length == 0 && (
+        <View style={{alignItems: "center", justifyContent:"center", alignContent: "center", top: 150}}> 
+          <Text style={styles.emptylistText}>You have no songs yet, start posting music for your friends!</Text>
+        </View>
+      )}
       <FlatList
-      data={songs}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({item}) => <MusicComponent item={item} savedSongs={false}/>}
-      contentContainerStyle={styles.flatListContainer} 
+        data={songs}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <MusicComponent item={item} savedSongs={false} />}
+        contentContainerStyle={styles.flatListContainer}
+        ListFooterComponent={renderFooter}
       />
     </View>
   );
@@ -42,14 +92,29 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: "#EBFFE9"
+    backgroundColor: '#EBFFE9',
+
   },
   text: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  
-  flatListContainer : {
-    paddingBottom: 120,
-  }
+  emptylistText: {
+    fontSize: 16,
+    fontFamily: 'Rubik-Medium',
+    color: '#A7A7A7',
+    textAlign: "center"
+  },
+
+  flatListContainer: {
+    paddingBottom: 140,
+  },
+  clearAllButton: {
+    alignItems: 'center',
+  },
+  clearAllText: {
+    fontSize: 16,
+    fontFamily: 'Rubik-SemiBold',
+    color: '#A7A7A7',
+  },
 });
